2f474e86f45c8f7284166f74873b429d440b6506
momozxmo <koomomozxmo@gmail.com>
2025-07-03 15:32:56 +0700

    lastert

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
-
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
+ 
 module.exports = defineConfig({
   projectId: "7qpyxu",
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
   e2e: {
     baseUrl: "https://automationexercise.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      mochawesome(on); // ✅ enable reporter plugin
     },
   },
 });
